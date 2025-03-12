166956186a8e46beb17ca688408707c651d42e86
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-13 02:40:04 +0600

    mongose removed

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,22 @@
-const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+
+const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+if (!uri) {
+  console.error("❌ MONGO_URI is not set in environment variables!");
+  process.exit(1);
+}
+
+const client = new MongoClient(uri);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await client.connect();
     console.log("✅ MongoDB Connected!");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error);
+    console.error("❌ MongoDB Connection Failed:", error.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+module.exports = { connectDB, client };

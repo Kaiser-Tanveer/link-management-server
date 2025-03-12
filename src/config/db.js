2b84099e243e807d01839208952d06139ec3fc7e
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-13 03:10:05 +0600

    env file updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+console.log("DB USER:", process.env.DB_USER);
+console.log("DB PASS:", process.env.DB_PASS);
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tl2ww1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
-if (!uri) {
-  console.error("❌ MONGO_URI is not set in environment variables!");
+if (!process.env.USER || !process.env.PASS) {
+  console.error("❌ MONGO_URI is missing USER or PASS in environment variables!");
   process.exit(1);
 }
 

0a01c33ee8ae5c9ee9465a4ac9466b4e0ab887a6
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-18 00:55:39 +0600

    link controller updated

--- src/Controllers/linkController.js
+++ src/Controllers/linkController.js
@@ -1,12 +1,14 @@
 const { ObjectId } = require('mongodb');
 
-// Add User 
+// Add Link
 const addLinks = async (req, res) => {
     try {
         const linkData = { ...req.body };
         console.log(linkData);
+
         const linkCollection = req.client.db('Link_Management').collection('links');
         const result = await linkCollection.insertOne(linkData);
+
         res.status(201).json({ message: `Link added successfully`, data: result });
     } catch (error) {
         console.error(`Error adding Account`, error);

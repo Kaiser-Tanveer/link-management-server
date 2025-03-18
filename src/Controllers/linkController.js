6423f3a67a288572da3fdfa4da149b9c315fe9ea
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-18 10:12:05 +0600

    get links controller added

--- src/Controllers/linkController.js
+++ src/Controllers/linkController.js
@@ -1,17 +1,27 @@
 const { ObjectId } = require('mongodb');
 
-// Add Link
+// Add own Link
+const linkCollection = req.client.db('Link_Management').collection('links');
 const addLinks = async (req, res) => {
     try {
         const linkData = { ...req.body };
-        const linkCollection = req.client.db('Link_Management').collection('links');
         const result = await linkCollection.insertOne(linkData);
-
         res.status(201).json({ message: `Link added successfully`, data: result });
     } catch (error) {
-        console.error(`Error adding Account`, error);
+        console.error(`Error adding link`, error);
         res.status(500).json({ error: `Failed to add link`});
     }
 };
 
-module.exports = { addLinks };
+// Get own links 
+const getLinks = async (req, res) => {
+    try {
+        const result = await linkCollection.find({}).toArray();
+        res.status(200).json({ message: `Links fetched successfully`, data: result });
+    } catch (error) {
+        console.error(`Error fetching links`, error);
+        res.status(500).json({ error: `Failed to fetch link`});
+    }
+};
+
+module.exports = { addLinks, getLinks };

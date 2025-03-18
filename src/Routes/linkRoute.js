3abc05894c574505faccbe3fbeca7bf0d1e0b57e
Kaiser-Tanveer <kaisertanveer0@gmail.com>
2025-03-18 10:13:12 +0600

    link route added

--- src/Routes/linkRoute.js
+++ src/Routes/linkRoute.js
@@ -4,5 +4,6 @@ const { addLinks } = require("../Controllers/linkController");
 const router = express.Router();
 
 router.post("/add", addLinks);
+router.get("/", addLinks);
 
 module.exports = router;
